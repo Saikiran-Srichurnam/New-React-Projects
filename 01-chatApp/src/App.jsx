bc511cf3933fabd225bb400886726cebd0deb9e5
saikiran <saikiransrichurnam628@gmail.com>
2025-08-14 07:21:20 +0530

    Created ChatApp folder

---
+++ 01-chatApp/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='w-full h-screen bg-gray-800'>
+      <div>Welcome to ChatApplication...</div>
+    </div>
+  )
+}
+
+export default App
